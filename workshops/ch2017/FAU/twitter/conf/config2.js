6476652730cbf2af05d14f03d2e85518845fd147
crishernandezmaps <crishmill@gmail.com>
2017-03-18 00:39:04 +0000

    no keys

--- workshops/ch2017/FAU/twitter/conf/config2.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'qmpakmaC1skuwwdiI9owi9nh4',
-  consumer_secret:      'OIqkEYKTSNPp9WNxnwSt7nW4GRbwFF9Q5HmNmVKNOukOwi04AG',
-  access_token:         '1081325768-UblPSFgN9fPIkvHvHIg11KBQdlDYarbOzzrsQx2',
-  access_token_secret:  '4MlzBSp2SL0yk9FNxBKzaOcNRR4TepbHB3gcZzfoIFkQI',
-}
--- workshops/ch2017/FAU/twitter/conf/config3.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'FTT7TaCntzpxhhrpRXWuSF8Z2',
-  consumer_secret:      'i9ajuMJ7hL4a4lyyPvzo4Q7lLLXnBQwOAY68a4zQFXxuPQzgLr',
-  access_token:         '1081325768-ZgFxj1H7O3iJ86C6270X7AOn0ok4YqcvPGGadeh',
-  access_token_secret:  'hCWrM2mciLFvrRtNQ7lX7T2o0vy0DUiZDq8KJOje7e2Da',
-}

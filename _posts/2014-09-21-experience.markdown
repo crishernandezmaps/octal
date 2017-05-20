5f8e2d0e84dac3dab6b807f64995377f72812d64
crishernandezmaps <crishmill@gmail.com>
2017-05-20 15:28:29 +0100

    slide

---
+++ _posts/2014-09-21-experience.markdown
@@ -0,0 +1,9 @@
+---
+layout: default
+category: Services
+title: Experience
+img: 
+gallery: true
+team: false
+---
+We want to create a game experience to scout cities around the world and collect light during the night. We will compare among those cities which is the most light polluted one.
--- js/landing-page.js
+++ js/landing-page.js
@@ -29,3 +29,19 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+// My own JS ////////////////////////////////////////
+
+if (document.documentElement.clientWidth < 900) {
+	// scripts
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  },  3000);
+}
